414d4eb6cbb5e6ba2019e41879d3f2a85639fbd2
whax1986 <alejandro.augusto.og@gmail.com>
2023-01-10 13:28:46 -0600

    First Commit

---
+++ UnoSquareProject/src/main/java/com/example/demo/UnoSquareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoSquareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoSquareProjectApplication.class, args);
+	}
+
+}
